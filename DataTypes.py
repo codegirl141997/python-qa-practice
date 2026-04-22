bd21edc747687d4e770f8cbd66063121dbd2c671
codegirl1411997 <dhanashreee14@gmail.com>
2026-04-22 07:02:51 +0530

    Python_Datatypes

---
+++ DataTypes.py
@@ -0,0 +1,55 @@
+# String
+x = "Hello"
+print(x)
+
+# Integer
+x = 10
+print(x)
+
+# Float
+x = 10.25
+print(x)
+
+# Complex
+x = 3 + 4j
+print(x)
+
+# List
+x = ["apple", "banana", "Cherry"]
+print(x)
+
+# Tuple
+x = ("radha", "jay", "krishna")
+print(x)
+
+# Range
+x = range(6)
+print(list(x))
+
+# Dictionary
+x = {"name": "Dhanashri", "age": 36}
+print(x)
+
+# Set
+x = {"apple", "banana", "cherry"}
+print(x)
+
+# Frozen Set
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+
+# Boolean
+x = True
+print(x)
+
+# Bytes
+x = b"hello"
+print(x)
+
+# Bytearray
+x = bytearray(5)
+print(x)
+
+# Memoryview
+x = memoryview(bytes(5))
+print(x)
