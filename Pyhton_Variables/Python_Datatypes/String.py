3cdfee5cc6bdcfdff38f6d577d7517c82e02b0c2
codegirl1411997 <dhanashreee14@gmail.com>
2026-06-30 16:03:48 +0530

    String

---
+++ Pyhton_Variables/Python_Datatypes/String.py
@@ -0,0 +1,26 @@
+'''
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:
+
+'''
+print (" Its's fine")
+print(" he is called it ")
+print('he is called john')
+
+# Assign String to a Variable
+a = "hello"
+print(a)
+
+# Multiline Strings
+
+a= """
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua
+"""
+
+print(a)
