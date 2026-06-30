eabb26c3b07f3cde6a035e3ac199e6bfda476f79
codegirl1411997 <dhanashreee14@gmail.com>
2026-06-30 12:10:47 +0530

    Updated Datatypes

--- Hello_World.py
+++ Hello_World.py
@@ -3,3 +3,16 @@ print ("Have a good day.")
 print("lEARN & Earn")
 
 print("Hello"); print("How are you?"); print("Bye bye!")
+
+a= 3
+print (a)
+
+
+str = "Hello World"
+print (str)
+
+
+
+b, c,d = 5,6.4,"Great"
+
+
